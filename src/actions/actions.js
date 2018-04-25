3f5411d111cf2a310425928f4c5be16ded575152
Marcin Spiewak <mspiewak85@gmail.com>
2018-04-25 09:14:26 +0100

    added dockerfile

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,11 +3,17 @@ import axios from "axios";
 
 export default function getBillsPerTenant(id) {
   return dispatch => {
-    axios.get("http://localhost:8090/tenant/" + id + "/bill").then(res => {
-      if (res.status === 200) {
-        dispatch(getBillsPerTenantAsync(res.data));
-      }
-    });
+    axios
+      .get(
+        "http://ec2-13-58-70-152.us-east-2.compute.amazonaws.com:8090/tenant/" +
+          id +
+          "/bill"
+      )
+      .then(res => {
+        if (res.status === 200) {
+          dispatch(getBillsPerTenantAsync(res.data));
+        }
+      });
   };
 }
 
